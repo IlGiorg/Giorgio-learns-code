c7dd0934108a917a3838c2544336823aa3fc0fdb
IlGiorg <97097944+IlGiorg@users.noreply.github.com>
2026-05-01 11:15:13 +0200

    ciaociaomaggio

---
+++ ciaociaomaggio/level2.py
@@ -0,0 +1,123 @@
+print("Welcome to Fired or Stay Hired")
+name = input("What is your name? ")
+print("Hello " + name + ", let's get started!")
+print("You are the principal of a school and it's time for teacher evaluations.")
+
+eval="Mr. Basilico & Bulfoni"
+scenario = "Mr. Basilico and mr. Bulfoni are two experienced teachers devoted to the students. They're known for being loud and messy, but they've always been good teachers. However, they have recently been caught touching each other inappropriately."
+print("\nYou are now evaluating " + eval)
+print(scenario)
+choice1=int(input("\nWhat do you say?\n1. YOU FAG..ntastic people ARE FIRED!\n2. I don't mind, I also touch my friends often."))
+
+eval="Mr. Ken"
+scenario = "Mr. Ken is a new teacher who has been with the school for only a year. He's extremely knowledgable and know what he's teaching, but some students say he's very queit and often doesn't teach enough."
+print("\nYou are now evaluating " + eval)
+print(scenario)
+choice2=int(input("\nWhat do you say?\n1. A teacher that doesn't teach? Not a teacher. Byebye\n2. I feel you, I also don't like teaching."))
+
+eval="Ms. Penelope & Ms. Alina"
+scenario = "Ms. Penelope and Ms. Alina have been reported from some class reps because: 'they haven't got a damn idea what they're teaching, these two incompetent' followed by some unkind words. When asked about it, they demonstrated thay actually do know what they're doing and even have high results"
+print("\nYou are now evaluating " + eval)
+print(scenario)
+choice3=int(input("\nWhat do you say?\n1. Never liked the two. FIRED\n2. That's some entitlement the students have."))   
+
+eval="Mr. Toma Pomo"
+scenario = "Mr. Toma Pomo is a teacher who has been with the school for several years. He's known for being strict and demanding, but some students say he's too harsh and doesn't give them enough support."
+print("\nYou are now evaluating " + eval)
+print(scenario)
+choice4=int(input("\nWhat do you say?\n1. Too harsh? I don't like harsh people\n2. Nasty students, be as harsh as you want")) 
+
+eval="Mr. Lucas"
+scenario = "Mr. Lucas is an amazing teacher, however he didn't show up to the performance review because 'I don't show up for tests dawg'. His teacher attendance is somehow 34%"
+print("\nYou are now evaluating " + eval)
+print(scenario)
+choice5=int(input("\nWhat do you say?\n1.THIRTY-FOUR PERCENT?! HOW? GET OUTTAHERE, DAWG\n2. Finally someone who gets me, like, it's not like I get paid to shopw up or something. Keep it up.")) 
+
+eval="Mr. Crippa"
+scenario = "nothing in particular, he just won't shut up talking about his spicy relationship with his 3d printer"
+print("\nYou are now evaluating " + eval)
+print(scenario)
+choice13=int(input("\nWhat do you say?\n1. I'm afraid to look at his print history. fired\n2. let's just say he's passionate. Not fired"))
+
+eval="Mr. Nespoli"
+scenario = "Mr. Nespoli is a teacher who looks chinese"
+print("\nYou are now evaluating " + eval)
+print(scenario)
+choice6=int(input("\nWhat do you say?\n1.ew, I don't want no dog eating almond eyes in my school\n2. Let's get the wives together and eat some ducks"))
+
+eval="Mr. Ludovic"
+scenario = "Mr. Ludovic has been known to be an amazing teacher, who always explains everything perfectly, but he's often quite direct with students who don't get it. 'Not my fault if they're stupid retards (he obviously means late, he's got french heritage)'"
+print("\nYou are now evaluating " + eval)
+print(scenario)
+choice7=int(input("\nWhat do you say?\n1.Support is the number one priority. Fired\n2. I don't like stupid retards(people that come in late) either, keep it up"))
+
+eval="Mr. Giorgio"
+scenario = "Mr. Giorgio is a teacher that knows the content very weel and is good at teaching it. But he brags every single damn second of the day that he's known the curriculum since he was born, annoying little brat."
+print("\nYou are now evaluating " + eval)
+print(scenario)
+choice8 =int(input("\nWhat do you say?\n1.fired, no one knows the curriculum better than I do\n2. Great job, keep going like this!"))
+
+eval="Mr. February"
+scenario = "Mr. February is a good teacher, the average Joe. Only thing is that the average Joe isn't found passed out drunk on the classroom floor from a student. Mr. February was found passed out drunk on the classroom floor from a student."
+print("\nYou are now evaluating " + eval)
+print(scenario)
+choice9=int(input("\nWhat do you say?\n1. get your drunk butt out of my school\n2. What's the problem with having fun? Keep it up!"))
+
+eval="Mr. March"
+scenario = "Mr. March is one of the most loved from students because he-s constantly pulling pranks on them. He's a history teacher and the deputy principal. His latest prank was putting a water bucket on a door. He waterbucketed a black, recently orphan, bullied student."
+print("\nYou are now evaluating " + eval)
+print(scenario)
+choice10=int(input("\nWhat do  say?\n1. That's the latest and last prank he's pulling\n2. HAHAHAHAHAHAHAHA GREAT PRANK BRO! TAKE A VIDEO NEXT TIME!"))
+
+eval="Ms. April"
+scenario = "Ms. April is one of the kinderarten teachers. Parents and students loved her, until one of the parents found her at a strip club drunk and completely naked. She was jumping from table to table flashing everyone."
+print("\nYou are now evaluating " + eval)
+print(scenario)
+choice11=int(input("\nWhat do you say?\n1.that's just crazy\n2. Let's keep her around. (did someone take pictures?????)"))
+
+eval="Mr. May"
+scenario = "Mr. May is hands down the best teacher of the school. He's known for being kind, supportive, actually knowing the content and how to explain it in a clear way andfor his quick and witty comebacks. Unfortunately, Mr. May has decided he want to teach in the caribbean. Oh and he was under arrest for smuggling 50 kilos of australian kangaroo beef."
+print("\nYou are now evaluating " + eval)
+print(scenario)
+choice12=int(input("\nWhat do you say?\n1. ciao ciao Mr. Maggio\n2. where can I get that beef?"))
+
+print("\n\nCongratulations " + name + ", you have completed the evaluations! Here are your results:")
+if choice1==1:
+    print("Mr. Basilico & Bulfoni: FIRED")
+else:    print("Mr. Basilico & Bulfoni: STAY HIRED")
+if choice2==1:
+    print("Mr. Ken: FIRED")
+else:    print("Mr. Ken: STAY HIRED")
+if choice3==1:
+    print("Ms. Penelope & Ms. Alina: FIRED")
+else:    print("Ms. Penelope & Ms. Alina: STAY HIRED")
+if choice4==1:
+    print("Mr. Toma Pomo: FIRED")
+else:    print("Mr. Toma Pomo: STAY HIRED")
+if choice5==1:
+    print("Mr. Lucas: FIRED")
+else:    print("Mr. Lucas: STAY HIRED")
+if choice13==1:
+    print("Mr. Crippa: FIRED")
+else:    print("Mr. Crippa: STAY HIRED")
+if choice6==1:
+    print("Mr. Nespoli: FIRED")
+else:    print("Mr. Nespoli: STAY HIRED")
+if choice7==1:
+    print("Mr. Ludovic: FIRED")
+else:    print("Mr. Ludovic: STAY HIRED")
+if choice8==1:
+    print("Mr. Giorgio: FIRED")
+else:    print("Mr. Giorgio: STAY HIRED")
+if choice9==1:
+    print("Mr. February: FIRED")
+else:    print("Mr. February: STAY HIRED")
+if choice10==1:
+    print("Mr. March: FIRED")
+else:    print("Mr. March: STAY HIRED")
+if choice11==1:
+    print("Ms. April: FIRED")
+else:    print("Ms. April: STAY HIRED")
+if choice12==1:
+    print("Mr. May: FIRED")
+else:    print("Mr. May: STAY HIRED")
